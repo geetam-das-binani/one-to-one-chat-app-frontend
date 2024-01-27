4bcc0ea759f8b2a4ab9c4fd7b313dbbf21cef5e5
Geetam <geetambinani6@gmail.com>
2024-01-27 14:49:05 +0530

    add typing logic

--- src/Chats.jsx
+++ src/Chats.jsx
@@ -7,13 +7,22 @@ import Button from "@mui/material/Button";
 import { auth } from "./firebase";
 import { signOut } from "firebase/auth";
 import ScrollableChats from "./ScrollableChats/ScrollableChats";
+import Lottie from "react-lottie";
 
+import animationData from "../src/animations/typing.json";
 // Initialize socket variable
 let socket;
-
+const defaultOptions = {
+  loop: true,
+  autoPlay: true,
+  animationData,
+  rendererSettings: {
+    preserveAspectRatio: "xMidYMid slice",
+  },
+};
 const Chats = () => {
-  const [val, setVal] = useState("");
-
+  const [newMessage, setNewMessage] = useState("");
+  const [isTyping, setIsTyping] = useState(false);
   const [messages, setMessages] = useState([]);
 
   const [userSocketId, setUserSocketId] = useState("");
@@ -26,27 +35,51 @@ const Chats = () => {
 
   const sendMessage = (e) => {
     e.preventDefault();
-    // Check if the message is not empty
-    if (!val) {
-      toast.error("Please enter something");
-      return;
-    }
 
     // Create message object
-    const msgObj = {
-      id: userSocketId,
-      name: user.name,
-      message: val,
-      pic: user.pic,
-      time: `${new Date(Date.now()).getHours()}:${new Date(
-        Date.now()
-      ).getMinutes()}`,
-    };
+    if (newMessage) {
+      // Create message object
+      const msgObj = {
+        id: userSocketId,
+        name: user.name,
+        message: newMessage,
+        pic: user.pic,
+        time: `${new Date(Date.now()).getHours()}:${new Date(
+          Date.now()
+        ).getMinutes()}`,
+      };
 
-    // Emit the message to the server
-    socket.emit("message", msgObj);
+      // Emit the message to the server
+      socket.emit("message", msgObj);
 
-    setVal("");
+      // Clear the input field after sending the message
+      setNewMessage("");
+      socket.emit("stop typing");
+    }
+  };
+  const typingHandler = (e) => {
+    setNewMessage(e.target.value);
+
+    if (e.target.value && userSocketId) {
+      socket.emit("typing");
+    } else {
+     
+      socket.emit("stop typing");
+    }
+    let lastTypingTime = new Date().getTime();
+    let timerLength = 3000;
+    setTimeout(() => {
+      let timeNow = new Date().getTime();
+      let timeDiff = timeNow - lastTypingTime;
+
+      if (timeDiff >= timerLength && isTyping) {
+        setIsTyping(false);
+        socket.emit("stop typing");
+      } 
+      else {
+        socket.emit("stop typing");
+      }
+    }, timerLength);
   };
 
   // Function to handle user logout
@@ -57,7 +90,7 @@ const Chats = () => {
   // useEffect for initializing the socket connection and event listeners
   useEffect(() => {
     // Connect to the socket server
-    socket = io("https://chatty-u9bf.onrender.com");
+    socket = io("http://localhost:3000");
 
     // Event listener for connection
     socket.on("connect", () => {
@@ -90,10 +123,13 @@ const Chats = () => {
 
   // useEffect to scroll to the last message when new messages are received
   useEffect(() => {
+    socket.on("typing", () => setIsTyping(true));
+    socket.on("stop typing", () => setIsTyping(false));
     lastMessageRef.current?.scrollIntoView();
     // The scrollIntoView() method scrolls an element into the visible area of the browser window.
   }, [messages]); // Run this effect when messages state changes
 
+ 
   return (
     <div>
       {/* Logout button */}
@@ -141,22 +177,38 @@ const Chats = () => {
           <div ref={lastMessageRef}></div>
         </main>
 
+        {isTyping && (
+          <div>
+            <Lottie
+                options={defaultOptions}
+               width={70}
+               style={{
+                marginRight:"15px",
+                marginLeft:"0px",
+                padding:".2rem",
+                background:"transparent"
+               }}
+               
+                
+               
+              
+            />
+          </div>
+        )}
         {/* Form for sending messages */}
-        <form className="msger-inputarea">
+        <form className="msger-inputarea" onSubmit={sendMessage}>
           <input
             type="text"
             className="msger-input"
             placeholder="Enter your message..."
-            value={val}
-            onChange={({ target }) => setVal(target.value)}
+            value={newMessage}
+            onChange={typingHandler}
+            onKeyUp={(e) => {
+              if (e.key === "Enter") {
+                sendMessage(e);
+              }
+            }}
           />
-          <button
-            onClick={sendMessage}
-            type="submit"
-            className="msger-send-btn"
-          >
-            Send
-          </button>
         </form>
 
         {/* Toast notification component */}
